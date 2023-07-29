ca99dd1634d6a1ccecbcb124aca5293b532f7858
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-07-29 20:10:59 +0200

    Add test for bug

--- test/samplers.jl
+++ test/samplers.jl
@@ -138,7 +138,7 @@ end
 end
 #! format: on
 
-@testset "Trajectory with CircularArraySARTSTraces and NStepBatchSampler" begin
+@testset "Trajectory with CircularPrioritizedTraces and NStepBatchSampler" begin
     n=1
     γ=0.99f0
 
@@ -149,6 +149,36 @@ end
                 state=Float32 => (4,),
             );
             default_priority=100.0f0
+        ),
+        sampler=NStepBatchSampler{SS′ART}(
+            n=n,
+            γ=γ,
+            batch_size=32,
+        ),
+        controller=InsertSampleRatioController(
+            threshold=100,
+            n_inserted=-1
+        )
+    )
+
+    push!(t, (state = 1, action = true))
+    for i = 1:9
+        push!(t, (state = i+1, action = true, reward = i, terminal = false))
+    end
+
+    b = RLTrajectories.StatsBase.sample(t)
+    @test haskey(b, :priority)
+end
+
+
+@testset "Trajectory with CircularArraySARTSTraces and NStepBatchSampler" begin
+    n=1
+    γ=0.99f0
+
+    t = Trajectory(
+        container=CircularArraySARTSTraces(
+                capacity=5,
+                state=Float32 => (4,),
         ),
         sampler=NStepBatchSampler{SS′ART}(
             n=n,
