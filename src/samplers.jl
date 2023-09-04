5ad92586685ec6964a8ed25dc01e0f07369af7e2
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-09-04 14:47:23 +0200

    edit docstring

--- src/samplers.jl
+++ src/samplers.jl
@@ -303,9 +303,10 @@ end
 """
     MultiStepSampler{names}(batchsize, stacksize, n, rng)
 
-A sampler that returns n consecutive steps of each trace. The samples are
-returned in an array of batchsize elements. For each element, n is truncated by the end
-of its episode. This means that the dimensions of each sample are not the same.  
+Sampler that fetches steps `[x, x+1, ..., x + n -1]` for each trace of each sampled index 
+`x`. The samples are returned in an array of batchsize elements. For each element, n is 
+truncated by the end of its episode. This means that the dimensions of each sample are not 
+the same.  
 """
 struct MultiStepSampler{names, S <: Union{Nothing,Int}, R <: AbstractRNG}
     n::Int
