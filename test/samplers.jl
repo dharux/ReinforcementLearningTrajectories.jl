fc9e02232a2d7eec9bd3c0caf93d5ae2854db11c
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-09-04 14:40:26 +0200

    add MultiStepSampler

--- test/samplers.jl
+++ test/samplers.jl
@@ -78,9 +78,9 @@ import ReinforcementLearningTrajectories.fetch
         γ = 0.99
         n_stack = 2
         n_horizon = 3
-        batch_size = 1000
+        batchsize = 1000
         eb = EpisodesBuffer(CircularArraySARTSATraces(capacity=10)) 
-        s1 = NStepBatchSampler(eb, n=n_horizon, γ=γ, stack_size=n_stack, batch_size=batch_size)
+        s1 = NStepBatchSampler(eb, n=n_horizon, γ=γ, stacksize=n_stack, batchsize=batchsize)
 
         push!(eb, (state = 1, action = 1))
         for i = 1:5
@@ -98,12 +98,12 @@ import ReinforcementLearningTrajectories.fetch
         for key in keys(eb)
             @test haskey(batch, key)
         end
-        #state: samples with stack_size
+        #state: samples with stacksize
         states = ReinforcementLearningTrajectories.fetch(s1, eb[:state], Val(:state), inds, ns[inds])
         @test states == [1 2 3 4 7 8 9;
                          2 3 4 5 8 9 10]
         @test all(in(eachcol(states)), unique(eachcol(batch[:state])))
-        #next_state: samples with stack_size and nsteps forward
+        #next_state: samples with stacksize and nsteps forward
         next_states = ReinforcementLearningTrajectories.fetch(s1, eb[:next_state], Val(:next_state), inds, ns[inds])
         @test next_states == [4 5 5 5 10 10 10;
                               5 6 6 6 11 11 11]
@@ -127,9 +127,9 @@ import ReinforcementLearningTrajectories.fetch
         ### CircularPrioritizedTraces and NStepBatchSampler
         γ = 0.99
         n_horizon = 3
-        batch_size = 4
+        batchsize = 4
         eb = EpisodesBuffer(CircularPrioritizedTraces(CircularArraySARTSATraces(capacity=10), default_priority = 10f0)) 
-        s1 = NStepBatchSampler(eb, n=n_horizon, γ=γ, batch_size=batch_size)
+        s1 = NStepBatchSampler(eb, n=n_horizon, γ=γ, batchsize=batchsize)
 
         push!(eb, (state = 1, action = 1))
         for i = 1:5
@@ -195,5 +195,49 @@ import ReinforcementLearningTrajectories.fetch
         @test length(b[1][:state]) == 4
         @test length(b[2][:state]) == 5
         @test !haskey(b[1], :action)
+    end
+    @testset "MultiStepSampler" begin
+        n_stack = 2
+        n_horizon = 3
+        batchsize = 1000
+        eb = EpisodesBuffer(CircularArraySARTSATraces(capacity=10)) 
+        s1 = MultiStepSampler(eb, n=n_horizon, stacksize=n_stack, batchsize=batchsize)
+
+        push!(eb, (state = 1, action = 1))
+        for i = 1:5
+            push!(eb, (state = i+1, action =i+1, reward = i, terminal = i == 5))
+        end
+        push!(eb, (state = 7, action = 7))
+        for (j,i) = enumerate(8:11)
+            push!(eb, (state = i, action =i, reward = i-1, terminal = false))
+        end
+        weights, ns = ReinforcementLearningTrajectories.valid_range(s1, eb)
+        @test weights == [0,1,1,1,1,0,0,1,1,1,0]
+        @test ns == [3,3,3,2,1,-1,3,3,2,1,0] #the -1 is due to ep_lengths[6] being that of 2nd episode but step_numbers[6] being that of 1st episode
+        inds = [i for i in eachindex(weights) if weights[i] == 1]
+        batch = sample(s1, eb)
+        for key in keys(eb)
+            @test haskey(batch, key)
+        end
+        #state and next_state: samples with stacksize
+        states =  ReinforcementLearningTrajectories.fetch(s1, eb[:state], Val(:state), inds, ns[inds])
+        @test states == [[1 2 3; 2 3 4], [2 3 4; 3 4 5], [3 4; 4 5], [4; 5;;], [7 8 9; 8 9 10], [8 9; 9 10], [9; 10;;]]
+        @test all(in(states), batch[:state])
+        #next_state: samples with stacksize and nsteps forward
+        next_states = ReinforcementLearningTrajectories.fetch(s1, eb[:next_state], Val(:next_state), inds, ns[inds])
+        @test next_states == [[2 3 4; 3 4 5], [3 4 5; 4 5 6], [4 5; 5 6], [5; 6;;], [8 9 10; 9 10 11], [9 10; 10 11], [10; 11;;]]
+        @test all(in(next_states), batch[:next_state])
+        #all other traces sample normally
+        actions = ReinforcementLearningTrajectories.fetch(s1, eb[:action], Val(:action), inds, ns[inds])
+        @test actions == [[2,3,4], [3,4,5], [4,5], [5], [8,9,10], [9,10],[10]]
+        @test all(in(actions), batch[:action])
+        next_actions = ReinforcementLearningTrajectories.fetch(s1, eb[:next_action], Val(:next_action), inds, ns[inds])
+        @test next_actions == [a .+ 1 for a in [[2,3,4], [3,4,5], [4,5], [5], [8,9,10], [9,10],[10]]]
+        @test all(in(next_actions), batch[:next_action])
+        rewards = ReinforcementLearningTrajectories.fetch(s1, eb[:reward], Val(:reward), inds, ns[inds])
+        @test rewards == actions
+        @test all(in(rewards), batch[:reward])
+        terminals = ReinforcementLearningTrajectories.fetch(s1, eb[:terminal], Val(:terminal), inds, ns[inds])
+        @test terminals == [[a == 5 ? 1 : 0 for a in acs] for acs in actions]
     end
 end
