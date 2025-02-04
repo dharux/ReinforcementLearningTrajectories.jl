b7089d7f01201b5fa7db7843363dc53ba50d8680
Dharanish <dharanish@evolbio.mpg.de>
2025-02-04 11:06:01 +0100

    Remove deepcopying in episodes
    Saves on allocation and clears in gc better

--- src/episodes.jl
+++ src/episodes.jl
@@ -59,12 +59,12 @@ function EpisodesBuffer(traces::AbstractTraces)
     if !isinf(cap)
         legalinds =  CircularBuffer{Bool}(cap)
         step_numbers = CircularBuffer{Int}(cap)
-        eplengths = deepcopy(step_numbers)
+        eplengths = CircularBuffer{Int}(cap)
         EpisodesBuffer(traces, legalinds, step_numbers, eplengths)
     else
         legalinds =  BitVector()
         step_numbers = Vector{Int}()
-        eplengths = deepcopy(step_numbers)
+        eplengths = Vector{Int}()
         EpisodesBuffer(traces, legalinds, step_numbers, eplengths)
     end
 end
